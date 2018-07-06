4b973de24357b20b2dbe0ddf43dc266514df2f95
Kanstantsin <kanstantsin.litvin@gmail.com>
2018-07-06 20:11:20 +0300

    1st commit

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
@@ -11,12 +6,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        /*Создать новую таблицу через php.
-Сделать страницу, где будет выводиться список таблиц текущей базы данных. В каждую таблицу можно зайти и увидеть название и тип поля.
-Добавить возможность удалить поле, изменить его тип или название.*/
-        
         require_once("connection.php");
-        
         $user='newuser';
         $pass='newpass';
         $table = 'address_book';
@@ -38,42 +28,12 @@ and open the template in the editor.
         $st->execute();
         $tables = $st->fetchAll(PDO::FETCH_ASSOC);
         //var_dump($tables);
-        
-        
-        
-        /*$q = $dbh->prepare("DESCRIBE ?");
-        $q->execute(array($table));
-        $table_fields = $q->fetchAll(PDO::FETCH_COLUMN);
-        var_dump($table_fields);*/
-
-        
-        
-        
-        
-        //$std = $db->prepare("DESCRIBE table_name");
-        //$std->execute();
-        //$rowsd = $std->fetch();
-        //$rowsd = $std->fetchAll(PDO::FETCH_ASSOC);
-        //var_dump($rowsd);
-        
-        
-        /*CREATE DATABASE db_name;// cоздание новой базы данных
-        CREATE TABLE `students` (
-`id` int NOT NULL AUTO_INCREMENT,
-`name` varchar(50) NULL,
-`estimation`float NOT NULL,
-`budget` tinyint(4) NOT NULL DEFAULT '0',
-PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;// создание новой таблицы
-    SHOW TABLES;//показать таблицы
-DESCRIBE table_name;*/
         ?>
         <h2>Доступные таблицы текущей базы данныx:</h2>
         <?php foreach( $tables as $value ){
-            foreach( $value as $val1 => $val2 ){
-                echo '<a href="tableInfo.php?val2='. $val2 .'">'. $val2 . '</a>';
+            foreach( $value as $key => $val ){
+                echo '<a href="tableInfo.php?val='. $val .'">'. $val . '</a>';
             }
         } ?>
-        
     </body>
 </html>
--- tableInfo.php
+++ tableInfo.php
@@ -8,24 +8,29 @@
     <body-->
         <?php
         require_once("connection.php");
-        $tablename = $_GET['val2'];
-        //echo $tablename;
+        $tablename = $_GET['val'];
         $q = $dbh->prepare("DESCRIBE $tablename");
         $q->execute();
         $table_fields = $q->fetchAll(PDO::FETCH_COLUMN);
-        //echo $table_fields;
+        //var_dump ($table_fields);
+        $p = $dbh->prepare("SELECT DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = ?");
+        $p->execute(array($tablename));
+        $fields_meta = $p->fetchAll(PDO::FETCH_COLUMN);
+        //var_dump ($fields_meta);
         echo '<h3>Доступные поля таблицы ' . $tablename . ':</h3>';
         ?>
 
-<table>
-    <tr>
-        <?php foreach( $table_fields as $field2 ){
-                echo '<th>'. $field2 . '</th>';
-        } ?>
-    <tr>
-    <tr>
-        
-    </tr>
-</table>
-    <!--/body>
-</html-->
+        <table>
+            <tr>
+            <?php foreach( $table_fields as $field ){
+                echo '<th>'. $field . '</th>';
+            } ?>
+            </tr>
+            <tr>
+            <?php foreach( $fields_meta as $meta ){
+                echo '<td>'. $meta . '</td>';
+            } ?>
+            </tr>
+        </table>
+    </body>
+</html>
